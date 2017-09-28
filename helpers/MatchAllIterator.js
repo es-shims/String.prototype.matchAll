ced8d25c8df6deb36109da714b2a44d2af9148d8
Jordan Harband <ljharb@gmail.com>
2017-09-28 10:45:30 -0400

    [Fix] detect “flags” itself, not just the property on the user-supplied value

--- helpers/MatchAllIterator.js
+++ helpers/MatchAllIterator.js
@@ -5,6 +5,7 @@ var flagsGetter = require('regexp.prototype.flags');
 
 var RegExpStringIterator = require('./RegExpStringIterator');
 var OrigRegExp = RegExp;
+var hasFlags = typeof (/a/).flags === 'string';
 
 module.exports = function MatchAllIterator(R, O) {
 	if (!ES.IsRegExp(R)) {
@@ -13,14 +14,16 @@ module.exports = function MatchAllIterator(R, O) {
 	var S = ES.ToString(O);
 	var C = ES.SpeciesConstructor(R, OrigRegExp);
 	var flags = ES.Get(R, 'flags');
+
 	var matcher;
-	if (typeof flags === 'string') {
-		matcher = new C(R, flags); // ES.Construct(C, [R, flags]);
+	var actualFlags = typeof flags === 'string' ? flags : flagsGetter(R);
+	if (hasFlags) {
+		matcher = new C(R, actualFlags); // ES.Construct(C, [R, actualFlags]);
 	} else if (C === OrigRegExp) {
 		// workaround for older engines that lack RegExp.prototype.flags
-		matcher = new C(R.source, flagsGetter(R)); // ES.Construct(C, [R.source, flagsGetter(R)]);
+		matcher = new C(R.source, actualFlags); // ES.Construct(C, [R.source, actualFlags]);
 	} else {
-		matcher = new C(R, flagsGetter(R)); // ES.Construct(C, [R, flagsGetter(R)]);
+		matcher = new C(R, actualFlags); // ES.Construct(C, [R, actualFlags]);
 	}
 	var lastIndex = ES.ToLength(ES.Get(R, 'lastIndex'));
 	ES.Set(matcher, 'lastIndex', lastIndex, true);
